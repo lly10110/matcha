b398c20e6917fd90e2ebf0695724c277e4de7c36
quam <yes>
2023-12-14 20:23:43 -0500

    app.post form submission

--- app.js
+++ app.js
@@ -26,9 +26,30 @@ app.get('/', (req, res) => {
 app.post('/process', (req, res) => {
     // Process your data here
     console.log(req.body);
-    res.send('Data received');
-});
-
+    const { bubbleTea, toppings } = req.body;
+    
+    res.send(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/style.css">
+        <title>Form Submission Confirmation</title>
+    </head>
+    <body>
+        <h1>what you want</h1>
+        
+        <p>Thank you for submitting the form. The following information was sent:</p>
+        
+        <ul>
+            <li>Bubble Tea: ${bubbleTea}</li>
+            <li>Toppings: ${toppings}</li>
+        </ul>
+        
+        <p>Click <a href="match.html">here</a> to go to the match page.</p>
+    </body>
+    </html>`);
+}); 
 
 // Add more routes as needed
 
