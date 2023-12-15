681936ee76653c1e3c3a9fcef920768f28228c29
quam <yes>
2023-12-14 19:33:15 -0500

    Middleware fix to allow public folder to be seen
    Co-authored-by: Kathleenvo0112 <Kathleenvo0112@users.noreply.github.com>

--- app.js
+++ app.js
@@ -2,16 +2,29 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const axios = require('axios');
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, '.env') }) 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+process.stdin.setEncoding("utf8");
+
+if (process.argv.length != 3) {
+    process.stdout.write(`Usage app.js portNumber`);
+    process.exit(1);
+  }
+
+let portNumber = (process.argv[2]);
+
 // Connect to MongoDB
-mongoose.connect('YOUR_MONGODB_CONNECTION_STRING', { useNewUrlParser: true, useUnifiedTopology: true });
+const uri = process.env.MONGO_CONNECTION_STRING;
+const databaseAndCollection = { db: 'CMSC335_DB', collection: 'matcha' };
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.get('/', (req, res) => {
@@ -23,5 +36,6 @@ app.get('/', (req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}: http://localhost:${PORT}`);
 });
+
