385dedda00cfa7789185909d6482e85e3cdff05b
quam <yes>
2023-12-15 00:08:34 -0500

    matching and post landing page working

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const axios = require('axios');
 const path = require("path");
+const crypto = require('crypto');
+const ejs = require('ejs');
+
 require("dotenv").config({ path: path.resolve(__dirname, '.env') }) 
 
 const app = express();
@@ -10,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
 const uri = process.env.MONGO_CONNECTION_STRING;
-const databaseAndCollection = { db: 'CMSC335_DB', collection: 'matcha' };
+const databaseAndCollection = { db: process.env.MONGO_DB_NAME, collection: process.env.MONGO_COLLECTION };
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,11 +24,27 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-app.post('/process', (req, res) => {
+app.post('/process', async (req, res) => {
     // Process your data here
-    console.log(req.body);
-    const { bubbleTea, toppings } = req.body;
+    let { bubbleTea, toppings } = req.body;
+    if (typeof bubbleTea === 'string') {
+        bubbleTea = [bubbleTea];
+    }
+    if (typeof toppings === 'string') {
+        toppings = [toppings];
+    }
+    const id = createUserID(bubbleTea + toppings);
+    const data = [];
+
+    for (let i = 0; i < 3; i++) {
+        const tea = getRandomTea(bubbleTea);
+        const topping = getRandomToppings(toppings);
+        data.push({ id: id, tea: tea, toppings: topping });
+    }
     
+    // Save the data to MongoDB
+    await postData(data);
+
     res.send(`<!DOCTYPE html>
     <html lang="en">
     <head>
@@ -43,13 +60,28 @@ app.post('/process', (req, res) => {
         
         <ul>
             <li>Bubble Tea: ${bubbleTea}</li>
-            <li>Toppings: ${toppings}</li>
+            <li>Toppings: ${toppings} </li>
         </ul>
         
-        <p>Click <a href="match.html">here</a> to go to the match page.</p>
+        <form action="/match" method="post">
+            <input type="hidden" name="id" value="${id}">
+            <button type="submit">View your matches!</button>
+        </form>
     </body>
     </html>`);
 }); 
+
+app.post('/match', async (req, res) => {
+    // Get data from MongoDB
+    const data = await getData(req.body.id);
+    console.log(data.length);
+    
+
+    // Render the match page
+    res.render(__dirname + '/public/match.ejs', { data: data });
+    // Delete data from MongoDB
+    await deleteData(req.body.id);
+});
 
 // Add more routes as needed
 
@@ -57,4 +89,82 @@ app.post('/process', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}: http://localhost:${PORT}`);
 });
+
+
+// post data to mongodb
+const { MongoClient, ServerApiVersion } = require('mongodb');
+async function postData(data) {
+    
+    const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB: Posting Data');
+        for (let i = 0; i < data.length; i++) {
+            const { id, tea, toppings } = data[i];
+            const result = await client.db(databaseAndCollection.db).collection(databaseAndCollection.collection).insertOne({ id: id, bubbleTea: tea, toppings: toppings });
+        }
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+// view data from mongodb
+async function getData(id) {
+    const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB: Getting Data');
+        const result = await client.db(databaseAndCollection.db).collection(databaseAndCollection.collection).find({id: id}).toArray();
+        console.log(result);
+        return result;
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+// delete data from mongodb
+async function deleteData(id) {
+    const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB');
+        const result = await client.db(databaseAndCollection.db).collection(databaseAndCollection.collection).deleteMany({ id: id });
+        console.log(result);
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+function createUserID(inputString) {
+    const hash = crypto.createHash('sha256');
+    hash.update(inputString);
+    return hash.digest('hex');
+}
+
+function getRandomTea(teas) {
+    if (teas.length <= 1) {
+        teas = ['Black Tea', 'Green Tea', 'Oolong Tea', 'Earl Grey', 'Jasmine Tea', 'Thai Tea', 'Matcha'];
+    }
+    return teas[Math.floor(Math.random() * teas.length)];
+}
+
+function getRandomToppings(toppings) {
+    let new_toppings = toppings;
+    if (toppings.length <= 1) {
+        new_toppings = ['Boba', 'Jelly', 'Pudding', 'Grass Jelly', 'Red Bean'];
+    }
+    let randomToppings = [];
+    const numToppings = Math.floor(Math.random() * new_toppings.length) + 1;
+    for (let i = 0; i < numToppings; i++) {
+        const randomIndex = Math.floor(Math.random() * new_toppings.length);
+        randomToppings.push(new_toppings[randomIndex]);
+    }
+    return randomToppings;
+}
 
